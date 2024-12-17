933c95fc873c17c5f7cfdafd3f30c3ece212545b
raptor <raptor@0xdeadbeef.info>
2024-12-17 15:03:37 +0100

    feat: first functioning string xref logic

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,8 @@ use std::path::Path;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use idalib::idb::IDB;
+use idalib::xref::XRefQuery;
+use idalib::Address;
 
 /// TODO
 static COUNTER: AtomicUsize = AtomicUsize::new(0);
@@ -89,13 +91,33 @@ pub fn run(filepath: &Path) -> anyhow::Result<usize> {
     }
 
     // Create a new output directory, returning an error if it already exists and it's not empty
-    let dirpath = filepath.with_extension("dec");
+    let dirpath = filepath.with_extension("str");
     println!("[*] Preparing output directory {dirpath:?}");
     if dirpath.exists() {
         fs::remove_dir(&dirpath).map_err(|_| anyhow::anyhow!("output directory already exists"))?;
     }
     fs::create_dir_all(&dirpath)?;
     println!("[+] Output directory is ready");
+
+    // Locate cross-references to strings in target binary
+    println!();
+    println!("[*] Finding cross-references to strings...");
+    for i in 0..idb.strings().len() {
+        let s = idb.strings().get_by_index(i).unwrap();
+        let addr = idb.strings().get_address_by_index(i).unwrap();
+
+        // TODO
+        println!("\n{addr:#x} {s:?} ");
+        get_xrefs(&idb, addr)?; // TODO handle error gracefully, don't stop
+
+        // TODO check decompiler license
+
+        /*
+        // Traverse XREFs and mark call locations
+        idb.first_xref_to(func.start_address(), XRefQuery::ALL)
+            .map_or(Ok(()), |cur| Self::traverse_xrefs(idb, &cur, &desc))
+         */
+    }
 
     // TODO: find strings, XREFs, mark? (in case use open_with above), use `haruspex::decompile_to_file`
 
@@ -108,4 +130,28 @@ pub fn run(filepath: &Path) -> anyhow::Result<usize> {
     }
 
     Ok(COUNTER.load(Ordering::Relaxed))
+}
+
+fn get_xrefs(idb: &IDB, addr: Address) -> anyhow::Result<()> {
+    let mut cur = idb
+        .first_xref_to(addr, XRefQuery::ALL)
+        .ok_or_else(|| anyhow::anyhow!("no xrefs to address {addr:#x}"))?;
+
+    loop {
+        // Print address with caller function name if available
+        let caller = idb
+            .function_at(cur.from())
+            .map_or("<unknown>".to_string(), |func| func.name().unwrap());
+        println!("{:#x} in {}", cur.from(), caller);
+        //println!("{:#x}", cur.from());
+
+        // TODO: only decompile functions, do it directly or with iterator, or collection?
+
+        match cur.next_to() {
+            Some(next) => cur = next,
+            None => break,
+        }
+    }
+
+    Ok(())
 }
